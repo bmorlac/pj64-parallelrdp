56e0e48db40674041c215eb17bfc37051e760c86
bmorlac <bmorlac@users.noreply.github.com>
2024-09-26 21:11:56 +0200

    Fix CMake files for MSVC build

--- src/parallel-rdp-standalone.cmake
+++ src/parallel-rdp-standalone.cmake
@@ -27,6 +27,7 @@ set(PARALLEL_RDP_SRC_FILES
     ${PARALLEL_RDP_STANDALONE_DIR}/vulkan/semaphore_manager.cpp
     ${PARALLEL_RDP_STANDALONE_DIR}/vulkan/shader.cpp
     ${PARALLEL_RDP_STANDALONE_DIR}/vulkan/texture_format.cpp
+    ${PARALLEL_RDP_STANDALONE_DIR}/util/arena_allocator.cpp
     ${PARALLEL_RDP_STANDALONE_DIR}/util/logging.cpp
     ${PARALLEL_RDP_STANDALONE_DIR}/util/thread_id.cpp
     ${PARALLEL_RDP_STANDALONE_DIR}/util/aligned_alloc.cpp
